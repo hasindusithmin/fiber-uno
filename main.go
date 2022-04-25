5b97dd9d0a5cf38d699450cdfb57cd7d58ac2065
hasindu sithmin <hasindusithmin64@gmail.com>
2022-04-25 18:02:52 +0000

    add /user/:name route

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/user/:name", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("Hello %s, Welcome to the Fiber!", c.Params("name"))
+		return c.SendString(msg)
 	})
 
 	app.Listen(":3000")
